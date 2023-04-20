1ab0bf2c33f6c99cb9225d82346507cb90436ad3
Elias Perez <perexlito@gmail.com>
2023-04-19 23:56:22 -0300

    modifies

--- src/database/config.js
+++ src/database/config.js
@@ -1,13 +1,16 @@
 import Sequelize from "sequelize"
 import 'dotenv/config'
+import mysql2 from 'mysql2'; // Needed to fix sequelize issues with WebPack
+
 const db = new Sequelize(
     process.env.DATABASE_NAME_DATABASE,
     process.env.DATABASE_USERNAME,
     process.env.DATABASE_PASSWORD,
     {
-    host:process.env.DATABASE_HOST,
-    dialect: 'mysql',
-})
+        dialect: 'mysql',
+        dialectModule: mysql2, // Needed to fix sequelize issues with WebPack
+        host: process.env.DB_HOST
+    })
 
 
 export default db
